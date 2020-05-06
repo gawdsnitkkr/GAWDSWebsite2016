52b1e85c33797cae8a0bf4bb5527148b87076204
Karan Gupta <karangupta199920@gmail.com>
2020-05-06 20:33:07 +0530

    Icon design change and only current members displayed

--- js/fetchMembers.js
+++ js/fetchMembers.js
@@ -13,19 +13,27 @@ request.onload = function () {
 function populateContent(jsonObj) {
   const noOfFields = 6;
   let noOfAlumni = jsonObj["feed"]["entry"].length / noOfFields - 1;
+  const date = new Date();
+  let members = [];
   for (let i = 1; i <= noOfAlumni; i++) {
-    let memberNameContainer =
-      '<div class="Name">' +
-      jsonObj["feed"]["entry"][i * noOfFields + 1]["content"]["$t"].split(" ")[0] +
-      "</div>";
-
-    let memberContainer =
-      '<div class="Member" data-image="aman.jpg" style="background-image: url(' +
-      jsonObj["feed"]["entry"][i * noOfFields + 2]["content"]["$t"] +
-      '); opacity: 1">' +
-      memberNameContainer +
-      "</div>";
-
-    $("#MemberContainer").append(memberContainer);
+    const batch = jsonObj["feed"]["entry"][i * noOfFields + 5]["content"]["$t"].split("-")[1];
+    if(batch > date.getFullYear() || (+batch === date.getFullYear() && date.getMonth() < 5)) {
+      const memberNameContainer = '<div class="Name">' + jsonObj["feed"]["entry"][i * noOfFields + 1]["content"]["$t"] + "</div>";
+      const linkedInUrl = jsonObj["feed"]["entry"][i * noOfFields + 4]["content"]["$t"];
+      const imageUrl = jsonObj["feed"]["entry"][i * noOfFields + 2]["content"]["$t"];
+      const memberContainer =
+        '<div class="Member" data-image="aman.jpg" style="background-image: url(' + imageUrl +'); opacity: 1; cursor: pointer;" onclick="window.open(\''+linkedInUrl+'\');">' +
+        memberNameContainer +
+        "</div>";
+      const name = jsonObj["feed"]["entry"][i * noOfFields + 1]["content"]["$t"].split(" ")[0];
+      members.push({"batch": batch, "data": memberContainer, "name": name});
+    }
   }
+  // sorting on the basis of batch and name
+  members.sort(function(a, b) {
+    return a.batch < b.batch ? -1 : a.batch > b.batch ? 1 : a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
+  });
+  for(const member of members)
+    $("#MemberContainer").append(member.data);
+  console.log(members);
 }
